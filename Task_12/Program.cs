19db40b78cd14c54377d2cc555cfaa39ec40fd95
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-12 21:49:45 +0300

    updated Task_12

---
+++ Task_12/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли второе число
+// кратным первому. Если число 2 не кратно числу 1, то
+// программа выводит остаток от деления.
+
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter first integer:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second integer:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int superDeleted = abNub(num1, num2);
+{
+    res=(num1/num2)%4;
+    return res;
+}
+if ()
