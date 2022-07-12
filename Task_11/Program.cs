245028f567d862b36682b80383f707e5d10ae899
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-12 21:30:08 +0300

    added Task_11

---
+++ Task_11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+
+Console.WriteLine("Hello, World!");
+
+int num = new Random ().Next(100,1000);
+// int firstDigit = num/100;
+// int secondDigit = num%10;
+int dif (int num)
+{
+    int a = num/100;
+    int b = num%10;
+    int res = a*10+b;
+}
+Console.WriteLine($"{num}->{res}");
