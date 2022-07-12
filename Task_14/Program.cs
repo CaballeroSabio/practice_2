6b72a2448524b692a508428de7f3c832271bb44a
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-12 22:04:43 +0300

    added Task_14

---
+++ Task_14/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23
+
+
+Console.WriteLine("Enter an integer: ");
+int Kratnoe(int num)
+{
+    int res1 = num%7;
+    int res2 = num%23;
+    if(res1==0 && res2==0) return 0;
+    return num;
+}
+
+int digit
